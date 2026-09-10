cb634e76feb72a79a55e5963017fc7efad342e6d
Lykx33 <igordb5472@users.noreply.github.com>
2026-09-10 13:55:26 +0300

    fix 05_initials_and_len.py

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,4 +1,3 @@
-s = input()
-f, i, o = s.split()
+f, i, o = input().split()
 print(f'Инициалы: {f[0].upper()}{i[0].upper()}{o[0].upper()}.')
-print(f'Длина (символов): {len(s)}')
+print(f'Длина (символов): {len(f) + len(i) + len(o) + 2}')
