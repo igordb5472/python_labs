a9943e6d2e98b2f918d9e0052715daf14c3877df
Lykx33 <igordb5472@users.noreply.github.com>
2026-09-10 13:44:08 +0300

    code lab01

---
+++ src/lab01/02_sum_avg.py
@@ -0,0 +1,2 @@
+a, b = map(float, input().replace(',', '.').split())
+print(f'sum={a + b}; avg={(a + b) / 2:.2f}')
---
+++ src/lab01/05_initials_and_len.py
@@ -0,0 +1,4 @@
+s = input()
+f, i, o = s.split()
+print(f'Инициалы: {f[0].upper()}{i[0].upper()}{o[0].upper()}.')
+print(f'Длина (символов): {len(s)}')
